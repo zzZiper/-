3a8d1dc256fe957af7b0fbe129c5f6fa1d09495f
ziper <374719418@qq.com>
2019-06-24 15:49:25 +0800

    购物车局部

---
+++ src/components/common/MemberBanner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Carousel } from 'antd-mobile';
+
+const Com = ({ memberbannerlist }) => {
+  return (
+  <Carousel
+    autoplay
+    infinite
+    style={{ display: 'inline-block', width: '100%', height: 200 }}
+  >
+    {memberbannerlist.map((val, index) => (
+        <img
+          src={ val.image.src }
+          alt=""
+          key=""
+          style={{ width: '100%', verticalAlign: 'top' }}
+        />
+    ))}
+  </Carousel>
+)}
+
+export default Com;
